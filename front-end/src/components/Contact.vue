b23e133e62e6a3a1aaa1a2fb2109078efd8989a4
Maegan Wilson <maegan@maeganwilson.com>
2017-12-11 12:09:40 -0600

    Added all three components. Step 2 of tutorial is done

---
+++ front-end/src/components/Contact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Contact Me</h1>
+  <p>
+    <ul>
+      <li>Email: {{email}}</li>
+      <li>Website: {{web}}</li>
+    </ul>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data(){
+    return {
+      email: 'maegan@maeganwilson.com',
+      web: 'maeganjwilson.github.com'
+    }
+  }
+}
+</script>
