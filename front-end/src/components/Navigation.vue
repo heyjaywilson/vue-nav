0f8da93d6b4aff6f506c5d465a417a365e3d0bb7
Maegan Wilson <maegan@maeganwilson.com>
2017-12-11 13:45:54 -0600

    All done

--- front-end/src/components/Navigation.vue
+++ front-end/src/components/Navigation.vue
@@ -2,11 +2,47 @@
   <div>
     <h2>Nav Bar</h2>
     <nav>
-      <router-link to="/home">Home</router-link>
+      <router-link class="spacing" v-for="routes in links" 
+      v-bind:key="routes.id"
+      :to="`${routes.page}`">{{routes.text}}</router-link>
     </nav>
   </div>
 </template>
 
 <script>
-
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      links: [
+        {
+          id: 0,
+          text: 'Hello World',
+          page:'/HelloWorld'
+        },
+        {
+          id: 1,
+          text: 'Home',
+          page:'/Home'
+        },
+        {
+          id: 2,
+          text: 'About',
+          page:'/About'
+        },
+        {
+          id: 3,
+          text: 'Contact',
+          page:'/Contact'
+        }
+      ]
+    }
+  }
+}
 </script>
+<style>
+.spacing {
+  margin-right: 10px;
+}
+</style>
+
